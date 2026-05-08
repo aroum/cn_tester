1720dadcaa5158d866feba0a26dc524c8fadd61d
aroum <aroum24@gmail.com>
2026-05-08 17:53:57 +0300

    Use P0_xx/P1_xx pin macro names
    Replace numeric PIN_### macros with explicit P0_xx / P1_xx names in nicenano variant headers for both master and target firmware. This clarifies port/bit mapping (P0.02..P0.31 and P1.00..P1.15) and standardizes naming across boards. Files changed: mcu_firmwares/master_firmware/boards/nicenano/variant.h and mcu_firmwares/target_firmware/boards/nicenano/variant.h. Note: LED_BUILTIN still references PIN_015 and may need to be updated to the new macro (P0_15) in a follow-up.

--- mcu_firmwares/master_firmware/boards/nicenano/variant.h
+++ mcu_firmwares/master_firmware/boards/nicenano/variant.h
@@ -21,60 +21,60 @@ extern "C"
 #define PINS_COUNT 48
 #define PIN_SERIAL1_TX  6   // P0.00
 #define PIN_SERIAL1_RX  8   // P0.01
-#define PIN_002  2   // P0.02
-#define PIN_003  3   // P0.03
-#define PIN_004  4   // P0.04
-#define PIN_005  5   // P0.05
-#define PIN_006  6   // P0.06
-#define PIN_007  7   // P0.07
-#define PIN_008  8   // P0.08
-#define PIN_009  9   // P0.09
-#define PIN_010  10  // P0.10
-#define PIN_011  11  // P0.11
-#define PIN_012  12  // P0.12
-#define PIN_013  13  // P0.13
-#define PIN_014  14  // P0.14
-#define PIN_015  15  // P0.15
-#define PIN_016  16  // P0.16
-#define PIN_017  17  // P0.17
-#define PIN_018  18  // P0.18
-#define PIN_019  19  // P0.19
-#define PIN_020  20  // P0.20
-#define PIN_021  21  // P0.21
-#define PIN_022  22  // P0.22
-#define PIN_023  23  // P0.23
-#define PIN_024  24  // P0.24
-#define PIN_025  25  // P0.25
-#define PIN_026  26  // P0.26
-#define PIN_027  27  // P0.27
-#define PIN_028  28  // P0.28
-#define PIN_029  29  // P0.29
-#define PIN_030  30  // P0.30
-#define PIN_031  31  // P0.31
-#define PIN_100  32  // P1.00
-#define PIN_101  33  // P1.01
-#define PIN_102  34  // P1.02
-#define PIN_103  35  // P1.03
-#define PIN_104  36  // P1.04 (SDA) (SPECIFY &Wire)
-#define PIN_105  37  // P1.05
-#define PIN_106  38  // P1.06 (SCL) (SPECIFY &Wire)
-#define PIN_107  39  // P1.07 (ss) (uncomment line 90 to use)
-#define PIN_108  40  // P1.08
-#define PIN_109  41  // P1.09
-#define PIN_110  42  // P1.10
-#define PIN_111  43  // P1.11 (SCK)
-#define PIN_112  44  // P1.12
-#define PIN_113  45  // P1.13 (MOSI)
-#define PIN_114  46  // P1.14
-#define PIN_115  47  // P1.15 (MISO)
+#define P0_02  2   // P0.02
+#define P0_03  3   // P0.03
+#define P0_04  4   // P0.04
+#define P0_05  5   // P0.05
+#define P0_06  6   // P0.06
+#define P0_07  7   // P0.07
+#define P0_08  8   // P0.08
+#define P0_09  9   // P0.09
+#define P0_10  10  // P0.10
+#define P0_11  11  // P0.11
+#define P0_12  12  // P0.12
+#define P0_13  13  // P0.13
+#define P0_14  14  // P0.14
+#define P0_15  15  // P0.15
+#define P0_16  16  // P0.16
+#define P0_17  17  // P0.17
+#define P0_18  18  // P0.18
+#define P0_19  19  // P0.19
+#define P0_20  20  // P0.20
+#define P0_21  21  // P0.21
+#define P0_22  22  // P0.22
+#define P0_23  23  // P0.23
+#define P0_24  24  // P0.24
+#define P0_25  25  // P0.25
+#define P0_26  26  // P0.26
+#define P0_27  27  // P0.27
+#define P0_28  28  // P0.28
+#define P0_29  29  // P0.29
+#define P0_30  30  // P0.30
+#define P0_31  31  // P0.31
+#define P1_00  32  // P1.00
+#define P1_01  33  // P1.01
+#define P1_02  34  // P1.02
+#define P1_03  35  // P1.03
+#define P1_04  36  // P1.04 (SDA) (SPECIFY &Wire)
+#define P1_05  37  // P1.05
+#define P1_06  38  // P1.06 (SCL) (SPECIFY &Wire)
+#define P1_07  39  // P1.07 (ss) (uncomment line 90 to use)
+#define P1_08  40  // P1.08
+#define P1_09  41  // P1.09
+#define P1_10  42  // P1.10
+#define P1_11  43  // P1.11 (SCK)
+#define P1_12  44  // P1.12
+#define P1_13  45  // P1.13 (MOSI)
+#define P1_14  46  // P1.14
+#define P1_15  47  // P1.15 (MISO)
 
 #ifndef LED_BUILTIN
-#define LED_BUILTIN PIN_015
+#define LED_BUILTIN P0_15
 #endif
 
 #ifndef LED_BLUE
-#define LED_BLUE PIN_114 //added so bluefruit compiles. not in use, 
-#endif                  //if you do wanna use it, change the PIN_114 to whatever you want
+#define LED_BLUE P1_14 //added so bluefruit compiles. not in use, 
+#endif                  //if you do wanna use it, change the P1_14 to whatever you want
 
 #define WIRE_INTERFACES_COUNT 1
 
